719060396483a1cacc990cac60f5ff213d135f89
Mjavaidnawab <57194178+Mjavaidnawab@users.noreply.github.com>
2020-09-20 17:24:52 +0400

    Added class 2

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1>Assalam o Aleikum React First App from Muhammad Javaid Nawab</h1>
+        <h2>PIAIC Boot Camp 2020 Class-2 Base-React-App</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
