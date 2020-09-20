465d26af8cb30ab5a3fa0ebf1ada31527314c17c
Mjavaidnawab <57194178+Mjavaidnawab@users.noreply.github.com>
2020-09-20 22:09:22 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
       <div>
         <h1>Assalam o Aleikum React First App from Muhammad Javaid Nawab</h1>
         <h2>PIAIC Boot Camp 2020 Class-2 Base-React-App</h2>
+        <h3>Github.com Repo: https://github.com/Mjavaidnawab/bc20k-class2-base-r-app</h3>
+        <h3>Surge URL      : http://bc20k-class2-base-r-app.surge.sh/</h3>
       </div>
     </div>
   );
